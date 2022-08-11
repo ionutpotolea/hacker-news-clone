e640be07847108b865c37bf0188b7009ac87477d
ionutpotolea <ionut.potolea@gmail.com>
2022-08-11 20:11:34 +0300

    Requesting data from API endpoint

--- pages/stories.js
+++ pages/stories.js
@@ -1,5 +1,40 @@
 import view from "../utils/view.js"
 
-export default function Stories(path){
-  view.innerHTML = `<div>${path}</div>`
+export default async function Stories(path){
+  const stories = await getStories(path)
+  const hasStories = stories.length > 0
+
+  view.innerHTML = `
+    <div>
+      ${
+        hasStories ?
+          stories.map(story => `${JSON.stringify(story)}<hr />`) :
+          "There are no stories."
+        }
+    </div>
+  `
 }
+
+async function getStories(path){
+  let urlPath
+  switch(path){
+    case '/':
+      urlPath = '/news'
+      break
+    case '/new':
+      urlPath = '/newest'
+      break
+    default:
+      urlPath = path
+  }
+
+  const result = await fetch(`https://node-hnapi.herokuapp.com${urlPath}`)
+  return result.ok ? await result.json() : []
+}
+
+// https://node-hnapi.herokuapp.com
+
+// / (Top) -> /news
+// /new (New) -> /newest 
+// /ask (Ask) -> /ask 
+// /show (Show) -> /show
